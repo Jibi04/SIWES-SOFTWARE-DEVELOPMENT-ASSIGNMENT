82245ba8960f72d6f371c43acae04d58041bb728
Jibi <jibibarjiphilip@gmail.com>
2026-09-03 06:49:58 +0100

    [refactor] remove redundant continue statements, keep session alive by asking if user wants to perform any other task.

--- Assignment-09-Shopping-Cart/shopping_cart.py
+++ Assignment-09-Shopping-Cart/shopping_cart.py
@@ -1,3 +1,5 @@
+from helper_functions import get_integer_input_from_user, should_continue
+
 def shopping_cart_system():
     products = []
     line = 40 * '*'
@@ -14,58 +16,42 @@ def shopping_cart_system():
     print(line)
 
     while True:
-        request = get_user_values(f'{options}:> ')
+        request = get_integer_input_from_user(f'{options}:> ')
         if request == 5:
             break
         elif request == 4:
             print('Item count: ', len(products))
-            if should_continue():
-                continue
-            else: break
+            if not should_continue():
+                break
 
         elif request == 1:
             product_name = input('Product Name: ').strip()
             products.append(product_name)
             print(f'{product_name} added to cart.')
-            if should_continue():
-                continue
-            else: break
+            if not should_continue():
+                break
 
         elif request == 2:
             product_name = input('Product Name: ').strip()
             if product_name not in products:
                 print(f'{product_name} not in cart.')
+            if not should_continue():
                 break
             else:
                 products.remove(product_name)
                 print(f'{product_name} removed from cart.')
-                if should_continue():
-                    continue
-                else: break
+                if not should_continue():
+                    break
         elif request == 3:
             if len(products) <= 0:
                 print('Your cart is empty.')
                 continue
             for i, item in enumerate(products):
                 print(f'{i}: {item}')
-            break
+            if not should_continue():
+                break
         else:
             print('Invalid response, (5) to exit')
-            continue
-        
-def should_continue():
-    response = int(input('Would you like to do anything else? (1 - YES, 0 - NO): '))
-    return bool(response)
-
-def get_user_values(msg: str):
-    while True:
-        try:
-            res = input(f'{msg}')
-            val = float(res)
-            return val
-        except ValueError:
-            print(f'Invalid Value expected an Integer/Decimal but got \'{type(res).__name__}\'')
-            continue
 
 if __name__ == '__main__':
     shopping_cart_system()
