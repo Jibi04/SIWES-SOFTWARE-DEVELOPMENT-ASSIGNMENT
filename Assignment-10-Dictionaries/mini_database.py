5c8897d86889c5c7786119bc190ea39d1db44f21
Jibi <jibibarjiphilip@gmail.com>
2026-09-03 06:53:14 +0100

    [refactor] remove redundant continue statements, keep session alive by asking if user wants to perform any other task.

--- Assignment-10-Dictionaries/mini_database.py
+++ Assignment-10-Dictionaries/mini_database.py
@@ -1,3 +1,5 @@
+from helper_functions import get_integer_input_from_user, get_valid_phone, get_valid_age, get_valid_email, should_continue
+
 DATABASE = {}
 def dictionary_database_system():
     line = 40 * '*'
@@ -12,7 +14,10 @@ def dictionary_database_system():
     print(line)
 
     while True:
-        request = get_user_values(f'{options}:> ')
+        request = get_integer_input_from_user(f'{options}:> ')
+        if request not in (1, 2, 5):
+            print('Please select a valid option')
+            continue
         if request == 5:
             break
         elif request == 1:
@@ -20,9 +25,8 @@ def dictionary_database_system():
             student_name = student_data.get('name', '').lower()
             DATABASE[student_name] = student_data
             print(f"'{student_name.capitalize()}' added.")
-            if should_continue:
-                continue
-            else: break
+            if not should_continue():
+                break
         elif request == 2:
             student_name = input('Student Name: ').lower()
             data = DATABASE.get(student_name)
@@ -31,17 +35,15 @@ def dictionary_database_system():
             else:
                 print(data)
 
-            if should_continue:
-                continue
-            else: break
-        
+            if not should_continue():
+                break
 
 def get_student_data():
     name = input('Student Name: ')
-    age = get_user_values('Age: ')
+    age = get_valid_age()
     course = input('Course: ')
-    phone = input('phone no: ')
-    email = validate_email()
+    phone = get_valid_phone()
+    email = get_valid_email()
     return {
         'name': name, 
         'age': int(age), 
@@ -49,28 +51,6 @@ def get_student_data():
         'phone': phone, 
         'email': email
     }
-
-def validate_email():
-    while True:
-        res = input(f'Email: ').lower().strip()
-        if '@' not in res:
-            print(f'Invalid Email address')
-            continue
-        return res
-    
-def get_user_values(msg: str):
-    while True:
-        try:
-            res = input(f'{msg}: ')
-            val = float(res)
-            return val
-        except ValueError:
-            print(f'Invalid Value expected an Integer/Decimal but got \'{type(res).__name__}\'')
-            continue
-
-def should_continue():
-    response = int(input('Would you like to do anything else? (1 - YES, 0 - NO): '))
-    return bool(response)
 
 if __name__ == '__main__':
     dictionary_database_system()
