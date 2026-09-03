29d5c3d71cf4e02cf7da427e5f3f372429de8664
Jibi <jibibarjiphilip@gmail.com>
2026-09-03 06:18:40 +0100

    [refactor] remove get_user_input function replace it with get_integer_input_from_user.

--- Assignment-04-Calculator/simple_calculator.py
+++ Assignment-04-Calculator/simple_calculator.py
@@ -1,6 +1,7 @@
+from helper_functions import get_integer_input_from_user
 def simple_calculator():
-    val1 = get_user_values('Enter First Number')
-    val2 = get_user_values('Enter Second Number')
+    val1 = get_integer_input_from_user('Enter First Number')
+    val2 = get_integer_input_from_user('Enter Second Number')
 
     addition = val1 + val2
     subtration = val1 - val2
@@ -18,17 +19,5 @@ modulus = {modulus}
 power = {power}
 """
     print(results)
-
-
-def get_user_values(msg: str):
-    while True:
-        try:
-            res = input(f'{msg}: ')
-            val = float(res)
-            return val
-        except ValueError:
-            print(f'Invalid Value expected an Integer/Decimal but got \'{type(res).__name__}\'')
-            continue
-        
 if __name__ == '__main__':
     simple_calculator()
