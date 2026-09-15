7fd62c6660798a90f139f199ae90c61e8ffb73b5
Jibi <jibibarjiphilip@gmail.com>
2026-09-15 20:48:34 +0100

    [refactor] adjusted file imports

--- Assignment-10-Dictionaries/mini_database.py
+++ Assignment-10-Dictionaries/mini_database.py
@@ -1,4 +1,5 @@
-from helper_functions import get_integer_input_from_user, get_valid_phone, get_valid_age, get_valid_email, should_continue
+from helper_functions import should_continue
+from validators import get_integer_input_from_user, get_and_validate_11_digit_phone, get_valid_age, get_and_validate_email
 
 DATABASE = {}
 def dictionary_database_system():
@@ -13,37 +14,41 @@ def dictionary_database_system():
     print('\t\tMENU\t\t')
     print(line)
 
-    while True:
-        request = get_integer_input_from_user(f'{options}:> ')
-        if request not in (1, 2, 5):
-            print('Please select a valid option')
-            continue
-        if request == 5:
-            break
-        elif request == 1:
-            student_data = get_student_data()
-            student_name = student_data.get('name', '').lower()
-            DATABASE[student_name] = student_data
-            print(f"'{student_name.capitalize()}' added.")
-            if not should_continue():
+    try:
+        while True:
+            request = get_integer_input_from_user(f'{options}:> ')
+            if request not in (1, 2, 5):
+                print('Please select a valid option')
+                continue
+            if request == 5:
                 break
-        elif request == 2:
-            student_name = input('Student Name: ').lower()
-            data = DATABASE.get(student_name)
-            if data is None:
-                print(f"Student not registered.")
-            else:
-                print(data)
+            elif request == 1:
+                student_data = get_student_data()
+                student_name = student_data.get('name', '').lower()
+                DATABASE[student_name] = student_data
+                print(f"'{student_name.capitalize()}' added.")
+                if not should_continue():
+                    break
+            elif request == 2:
+                student_name = input('Student Name: ').lower()
+                data = DATABASE.get(student_name)
+                if data is None:
+                    print(f"Student not registered.")
+                else:
+                    print(data)
 
-            if not should_continue():
-                break
+                if not should_continue():
+                    break
+    except KeyboardInterrupt:
+        print("\nExiting...")
+        return
 
 def get_student_data():
     name = input('Student Name: ')
     age = get_valid_age()
     course = input('Course: ')
-    phone = get_valid_phone()
-    email = get_valid_email()
+    phone = get_and_validate_11_digit_phone()
+    email = get_and_validate_email()
     return {
         'name': name, 
         'age': int(age), 
