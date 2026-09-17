0f7062790d0c1504401cdd8d1fa76a60a38a7fff
Jibi <jibibarjiphilip@gmail.com>
2026-09-17 11:40:31 +0100

    bug-fix: fix import

--- Assignment-03-Input/student_registration_system.py
+++ Assignment-03-Input/student_registration_system.py
@@ -1,4 +1,4 @@
-from helper_functions import get_valid_email, get_integer_input_from_user, get_valid_phone, get_valid_age
+from validators import get_and_validate_email, get_and_validate_11_digit_phone, get_valid_age
 
 def student_registration_system():
     line = 50 * '='
@@ -7,8 +7,8 @@ def student_registration_system():
     age = get_valid_age()
     school = input('school: ').lower().strip()
     department = input('Department: ').lower().strip()
-    email = get_valid_email()
-    phone = get_valid_phone()
+    email = get_and_validate_email()
+    phone = get_and_validate_11_digit_phone()
 
     registration_completion_msg = f"""
 Welcome {name.capitalize()},
